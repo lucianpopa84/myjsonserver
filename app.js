0b9de87117dda4e26a032c080972c77cf15e6125
lucianpopa84 <lucianpopa84@gmail.com>
2019-04-19 17:50:21 +0300

    Update app, index, style

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 const displayRestaurantsButton = document.querySelector("#displayRestaurants");
+const displayRestaurantsByCityButton = document.querySelector("#displayRestaurantsByCity");
 const searchForm = document.querySelector("#searchForm");
 const searchFormInput = document.querySelector("#searchFormInput");
 const resultsList = document.querySelector("#resultsList");
 const restaurantDataTable = document.querySelector("#restaurantDataTable");
+const restaurantsListTitle = document.querySelector("#restaurantsListTitle");
+const restaurantDetailsTitle = document.querySelector("#restaurantDetailsTitle");
+const cityDelivery = document.querySelector("#cityDeliveryListInput");
 const numItems = 10; // number of items to display
 
-searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-});
-
-displayRestaurantsButton.addEventListener("click", () => display());
-
 // display restaurants
-function display(){
+function display() {
     let url = `https://my-json-server.typicode.com/lucianpopa84/myjsonserver/restaurants`;
     getRestaurants(url);
-  }
+} 
+
+// display restaurants by city
+function displayByCity() {
+    let url = `https://my-json-server.typicode.com/lucianpopa84/myjsonserver/restaurants/?location=${locality}`;
+    getRestaurants(url);
+} 
 
 // search restaurants
-function search(){
-  let inputValue = searchFormInput.value;
-  console.log("inputValue: ",inputValue);
-  let url = `https://my-json-server.typicode.com/lucianpopa84/myjsonserver/restaurants/${inputValue}`;
-  displayRestaurantData(url);
+function search() {
+    let inputValue = searchFormInput.value;
+    console.log("inputValue: ", inputValue);
+    let url = `https://my-json-server.typicode.com/lucianpopa84/myjsonserver/restaurants/${inputValue}`;
+    displayRestaurantData(url);
 }
 
 // ======= fetch json db =======
-function getRestaurants(url){
-  fetch(url)
-  .then(response => response.json())
-  .then(restaurantsData => {
-      let listHtmlContent =`
+function getRestaurants(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(restaurantsData => {
+            restaurantsListTitle.innerHTML = "Restaurants: ";
+            resultsList.innerHTML = "Loading data...";
+            let listHtmlContent = `
         <tr>
             <th>Id</th>
             <th>Name</th> 
@@ -39,9 +45,9 @@ function getRestaurants(url){
             <th>Working hours</th>
         </tr>
         `;
-      for (let i of restaurantsData.keys()){
-          if (i <= numItems) {
-            listHtmlContent += `
+            for (let i of restaurantsData.keys()) {
+                if (i <= numItems) {
+                    listHtmlContent += `
                 <tr>
                     <td>${restaurantsData[i].name}</td>
                     <td>${restaurantsData[i].id}</td>
@@ -49,17 +55,19 @@ function getRestaurants(url){
                     <td>${restaurantsData[i].location}</td>
                     <td>${restaurantsData[i].operHour} - ${restaurantsData[i].closeHour}</td>
                 </tr>`;
-          }
-      }
-      resultsList.innerHTML = listHtmlContent;
-  }).catch(error => console.log("error: ", error.message));
+                }
+            }
+            resultsList.innerHTML = listHtmlContent;
+        }).catch(error => console.log("error: ", error.message));
 }
 
-function displayRestaurantData(url){
+function displayRestaurantData(url) {
     fetch(url)
-    .then(response => response.json())
-    .then(restaurantData => {
-        let listHtmlContent = `
+        .then(response => response.json())
+        .then(restaurantData => {
+            restaurantDetailsTitle.innerHTML = "Restaurant data: ";
+            restaurantDataTable.innerHTML = "Loading data...";
+            let listHtmlContent = `
             <tr>
                 <th>Name:</th>
                 <td>${restaurantData.name}</td>
@@ -70,14 +78,14 @@ function displayRestaurantData(url){
             </tr>
             <tr>
                 <th rowspan="${restaurantData.foodType.length}">Food type:</th>`
-            for (let i of restaurantData.foodType.keys()){
+            for (let i of restaurantData.foodType.keys()) {
                 listHtmlContent += `
                 <td>${restaurantData.foodType[i]}</td>
                 </tr>
                 <tr>
                 `
             }
-            listHtmlContent +=`
+            listHtmlContent += `
             <tr>
                 <th>Location:</th>
                 <td>${restaurantData.location}</td>
@@ -89,5 +97,27 @@ function displayRestaurantData(url){
             `;
 
             restaurantDataTable.innerHTML = listHtmlContent;
-    }).catch(error => console.log("error: ", error.message));
-  }
+        }).catch(error => console.log("error: ", error.message));
+}
+
+
+function attachEvents() {
+    searchForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    displayRestaurantsButton.addEventListener("click", () => display());
+
+    displayRestaurantsByCityButton.addEventListener("click", () => displayByCity());
+
+    cityDelivery.onchange = () => {
+        locality = cityDelivery.value;
+    };
+
+    cityDelivery.onclick = () => {
+        cityDelivery.value = "";
+    };
+}
+
+attachEvents();
+
